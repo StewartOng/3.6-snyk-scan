0ea4ae3a5a125387d7cf1232505170ddc156b2cc
stewartong <ong.hueh.chuah@gmail.com>
2025-04-18 21:41:06 +0800

    Fix: Format Terraform code

--- main.tf
+++ main.tf
@@ -43,10 +43,10 @@ data "archive_file" "lambda_zip" {
 }
 
 resource "aws_lambda_function" "hello_world" {
-  function_name = "hello_world_lambda"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_exec.arn
-  handler       = "lambda_function.lambda_handler"
-  filename      = data.archive_file.lambda_zip.output_path
+  function_name    = "hello_world_lambda"
+  runtime          = "python3.9"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "lambda_function.lambda_handler"
+  filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
